e266fc8c6d85dc7ba12d1823f88d4270cc598e7b
thadler <t.hadler@aol.de>
2022-11-04 18:05:16 +0100

    updating aha model plots

--- src/LazyLuna/Figures/T1_diff_bullseye_plot.py
+++ src/LazyLuna/Figures/T1_diff_bullseye_plot.py
@@ -1,24 +1,19 @@
 import os
 import traceback
 
-from matplotlib import gridspec, colors, cm
-from matplotlib.figure import Figure
-from matplotlib.collections import PathCollection
-from mpl_interactions import ioff, panhandler, zoom_factory
 import matplotlib.pyplot as plt
-import seaborn as sns
+from matplotlib import colors, cm
+from matplotlib import path
+from matplotlib.patches import PathPatch
+from matplotlib.collections import PatchCollection
 
-import shapely
-from shapely.geometry import Polygon
-from descartes import PolygonPatch
-from scipy.stats import probplot
+from shapely.geometry import Polygon, Point
 import numpy as np
 import pandas
 
-from LazyLuna.Tables import *
-from LazyLuna.Metrics import *
-from LazyLuna import utils
 from LazyLuna.Figures.Visualization import *
+
+
 
 
 class T1_diff_bullseye_plot(Visualization):
@@ -28,7 +23,34 @@ class T1_diff_bullseye_plot(Visualization):
         self.canvas = canvas
         self.add_annotation = True
         
-    def visualize(self, segBold=[], minv=None, maxv=None):
+    def plot_polygon(self, ax, poly, **kwargs):
+        p = path.Path.make_compound_path(path.Path(np.asarray(poly.exterior.coords)[:, :2]), *[path.Path(np.asarray(ring.coords)[:, :2]) for ring in poly.interiors])
+        collection = PatchCollection([PathPatch(p, **kwargs)], **kwargs)
+        ax.add_collection(collection, autolim=True)
+        return collection
+
+    def write_val(self, ax, mean, std, x, y):
+        mean, std = '{:.1f}'.format(float(mean)), '({:.1f})'.format(float(std))
+        ax.annotate(mean + '\n' + str(std), xy = (x,y), xytext = (x,y), textcoords = 'data',
+                    bbox = dict(boxstyle='round', fc='w', edgecolor='w'), size = 10, 
+                    horizontalalignment = 'center', verticalalignment = 'center')
+        
+    def segment(self, center, st_angle, end_angle, radius_st, radius_end, steps=500):
+        def polar_p(op, a,  dist): # origin point, angle, distance
+            return [op.x + dist*np.sin(np.radians(a)), op.y + dist*np.cos(np.radians(a))]
+        st_angle %= 360
+        step_angle_width = (end_angle - st_angle) / steps
+        segment_vertices = [polar_p(center, st_angle, radius_st), polar_p(center, st_angle, radius_end)]
+        for step in range(1, steps): 
+            segment_vertices.append((polar_p(center, st_angle + step*step_angle_width, radius_end)))
+        segment_vertices.extend([polar_p(center, end_angle, radius_end), polar_p(center, end_angle, radius_st)])
+        for step in range(1, steps): 
+            segment_vertices.append((polar_p(center, end_angle-step*step_angle_width, radius_st)))
+        segment_vertices.append((polar_p(center, st_angle, radius_st)))
+        return Polygon(segment_vertices)
+    
+    
+    def visualize(self):
         """Plots a bullseye differences plot for a case comparison in mapping view
         
         Note:
@@ -44,85 +66,29 @@ class T1_diff_bullseye_plot(Visualization):
         means1, stds1 = cat1.calc_mapping_aha_model()
         means2, stds2 = cat2.calc_mapping_aha_model()
 
-        means = np.concatenate((means1[0]-means2[0],
-                                means1[1]-means2[1],
-                                means1[2]-means2[2]))
-        stds  = np.concatenate((stds1[0]-stds2[0], 
-                                stds1[1]-stds2[1], 
-                                stds1[2]-stds2[2]))
-        
-        ax = self.subplots(1,1, subplot_kw=dict(projection='polar'))
-        cmap = plt.cm.PRGn
-        if minv is None: minv=np.min(means)-5
-        if maxv is None: maxv=np.max(means)+5
-        minv, maxv = min([minv, -maxv]), max([maxv, -minv])
-        norm = colors.Normalize(vmin=minv, vmax=maxv)
-        means = np.array(means).ravel()
-        stds  = np.array(stds) .ravel()
-        theta = np.linspace(0, 2*np.pi, 768)
-        r = np.linspace(0.2, 1, 4)
-        linewidth = 2
-        for i in range(r.shape[0]): ax.plot(theta, np.repeat(r[i], theta.shape), '-k', lw=linewidth)
-        for i in range(6):
-            theta_i = i * 60 * np.pi/180
-            ax.plot([theta_i, theta_i], [r[1], 1], '-k', lw=linewidth)
-        for i in range(4):
-            theta_i = i * 90 * np.pi/180 - 45*np.pi/180
-            ax.plot([theta_i, theta_i], [r[0], r[1]], '-k', lw=linewidth)
-        r0 = r[2:4]
-        r0 = np.repeat(r0[:,np.newaxis], 128, axis=1).T
-        for i in range(6):
-            theta0 = theta[i*128:i*128+128] + 60*np.pi/180 #+ 60*np.pi/180
-            theta0 = np.repeat(theta0[:,np.newaxis], 2, axis=1)
-            self.write_val(ax, means[i], stds[i], i*60*np.pi/180 + 30*np.pi/180 + 60*np.pi/180, np.mean(r0[0]))
-            z = np.ones((128,2)) * means[i]
-            ax.pcolormesh(theta0, r0, z, cmap=cmap, norm=norm)
-            if i+1 in segBold:
-                ax.plot(theta0, r0, '-k', lw=linewidth+2)
-                ax.plot(theta0[0 ], [r[2],r[3]], '-k', lw=linewidth+1)
-                ax.plot(theta0[-1], [r[2],r[3]], '-k', lw=linewidth+1)
-        r0 = r[1:3]
-        r0 = np.repeat(r0[:,np.newaxis], 128, axis=1).T
-        for i in range(6):
-            theta0 = theta[i*128:i*128+128] + 60*np.pi/180 #+ 60*np.pi/180
-            theta0 = np.repeat(theta0[:,np.newaxis], 2, axis=1)
-            self.write_val(ax, means[i+6], stds[i+6],  i*60*np.pi/180 + 30*np.pi/180 + 60*np.pi/180, np.mean(r0[0]))
-            z = np.ones((128,2)) * means[i+6]
-            ax.pcolormesh(theta0, r0, z, cmap=cmap, norm=norm)
-            if i+7 in segBold:
-                ax.plot(theta0, r0, '-k', lw=linewidth+2)
-                ax.plot(theta0[0 ], [r[1],r[2]], '-k', lw=linewidth+1)
-                ax.plot(theta0[-1], [r[1],r[2]], '-k', lw=linewidth+1)
-        r0 = r[0:2]
-        r0 = np.repeat(r0[:,np.newaxis], 192, axis=1).T
-        for i in range(4):
-            theta0 = theta[i*192:i*192+192] + 45*np.pi/180  #+ 90*np.pi/180 
-            theta0 = np.repeat(theta0[:,np.newaxis], 2, axis=1)
-            self.write_val(ax,means[i+12], stds[i+12], i*90*np.pi/180 + 90*np.pi/180, np.mean(r0[0]))
-            z = np.ones((192,2)) * means[i+12]
-            ax.pcolormesh(theta0, r0, z, cmap=cmap, norm=norm)
-            if i+13 in segBold:
-                ax.plot(theta0, r0, '-k', lw=linewidth+2)
-                ax.plot(theta0[0 ], [r[0],r[1]], '-k', lw=linewidth+1)
-                ax.plot(theta0[-1], [r[0],r[1]], '-k', lw=linewidth+1)
-        ax.set_ylim([0, 1])
-        ax.set_yticklabels([])
-        ax.set_xticklabels([])
-        axp    = ax.imshow(np.random.randint(0, 100, (100, 100)))
-        cbaxes = self.add_axes([0.78, 0.1, 0.03, 0.8])  # This is the position for the colorbar
-        cb     = self.colorbar(cm.ScalarMappable(cmap=cmap, norm=norm), ax=axp, cax=cbaxes)
-        
-        self.canvas.draw()
+        means = np.concatenate((means1[0]-means2[0], means1[1]-means2[1], means1[2]-means2[2]))
+        stds  = np.concatenate((stds1[0] -stds2[0],  stds1[1] -stds2[1],  stds1[2] -stds2[2]))
 
-    def write_val(self, ax, mean, std, angle, y):
-        mean = "{:.1f}".format(float(mean))
-        std  = "{:.1f}".format(float(std))
-        ax.annotate(str(mean) + '\n(' + str(std) + ')',
-                xy                  = (angle, y), # theta, radius
-                xytext              = (angle, y), # fraction, fraction
-                textcoords          = 'data',     #'figure fraction',
-                bbox                = dict(boxstyle="round", fc="1.0", edgecolor="1.0"),
-                horizontalalignment = 'center',
-                size                = 10,
-                verticalalignment   = 'center',
-                )
+        cmap = plt.cm.PRGn
+        norm = colors.Normalize(vmin=np.min(means), vmax=np.max(means))
+        
+        ax = self.subplots(1,1)#, subplot_kw=dict(projection='polar'))
+        ax.imshow(np.ones((240,280)), vmin=0, vmax=1, cmap='gray'); ax.axis('off')
+
+        # plot segments with colors
+        center = Point(120,120)
+        basal_segments = [self.segment(center, 30+60*i, 30+60*(i+1), 80, 110) for i in range(6)]
+        midv_segments  = [self.segment(center, 30+60*i, 30+60*(i+1), 50, 80)  for i in range(6)]
+        apex_segments  = [self.segment(center, 45+90*i, 45+90*(i+1), 20, 50)  for i in range(4)]
+        segments = basal_segments + midv_segments + apex_segments
+        for i_p, p in enumerate(segments): self.plot_polygon(ax, p, color=cmap(norm(means[i_p])), ec='k', lw=2.5)
+        cbaxes = self.add_axes([0.79, 0.24, 0.03, 0.5])
+        cb = self.colorbar(cm.ScalarMappable(cmap=cmap, norm=norm), ax=ax, cax=cbaxes)
+
+        # write values onto segment patches # (basal, midv, apex)
+        xs = [120,38,38,120,202,202, 120,63,63,120,178,178, 120,85,120,155]
+        ys = [25,71,169,215,169,71,  55,90,150,185,150,90,  85,120,155,120]
+        for mean,std, x,y in zip(means, stds, xs,ys): self.write_val(ax, mean, std, x, y)
+        
+        ax.set_title('AHA Model Difference [ms]: '+self.cc.case1.reader_name+' - '+self.cc.case2.reader_name)
+        self.canvas.draw()
